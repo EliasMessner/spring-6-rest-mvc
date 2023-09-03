da047f03039cb55cd893de597684aa0c64eea785
EliasMessner <elias.messner@unite.eu>
2023-09-03 14:06:42 +0200

    add post method

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -3,7 +3,11 @@ package guru.springframework.spring6restmvc.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +23,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+    @PostMapping
+    public ResponseEntity<Beer> handlePost(@RequestBody Beer beer) {
+        beerService.saveNewBeer(beer);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
